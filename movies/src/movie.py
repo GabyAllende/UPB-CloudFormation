8ef4d0ff863957e7e2921c6e6e6b6698193365b7
GabyAllende <gabrielaallende158@gmail.com>
2021-06-16 08:10:32 +0000

    MASTER: practice finished

--- movies/src/movie.py
+++ movies/src/movie.py
@@ -129,45 +129,136 @@ def getAudience(event, context):
                 temp.append(y)
     
     
-    print(temp)
-    
-    
-    # response = table.get_item(
-    #     Key={
-    #         'pk': user_id,
-    #         'sk': 'age'
-    #     }
-    # )
-    # item = response['Item']
-    # print(item)
     return {
         'statusCode': 200,
-        'body': json.dumps(data1)
+        'body': json.dumps(temp)
     }
     
     
-def getAvailableRooms2(event, context):
+def getAvailableSeats3D(event, context):
     print(json.dumps({"running": True}))
     print(json.dumps(event))
     
     path = event["path"] # "/user/123"
     array_path = path.split("/") # ["", "user", "123"]
-    movie_id = array_path[-1]
-    print("Llega hasta aca")
+    room_id = array_path[-1]
     
-    table2 = table.scan(FilterExpression=Attr('pk').eq(movie_id) & Attr('sk').begins_with("room_"))
-    data = table2['Items']
-    print("data")
-    # response = table.get_item(
-    #     Key={
-    #         'pk': user_id,
-    #         'sk': 'age'
-    #     }
-    # )
-    # item = response['Item']
-    # print(item)
+    
+    q1 = table.scan(FilterExpression=Attr('pk').eq(room_id) & Attr('sk').eq("room_info"))
+    q2 = table.scan(FilterExpression=Attr('pk').eq(room_id) & Attr('sk').begins_with("person_"))
+    asientosOcupados = len(q2['Items'])
+    capacidadRoom = q1['Items']
+    a=""
+    print('CapacidadRomm: ',capacidadRoom)
+    print('CapacidadRomm[0][capacity]: ',capacidadRoom[0]['capacity'])
+    
+    
+    
+    if len(capacidadRoom)>0:
+        cap = capacidadRoom[0]['capacity']
+        a = "El numero de asientos libres del la sala: "+room_id+" es de "+str(int(cap)-asientosOcupados)
+    else:
+        a = "No se encontro la informacion para la sala "+room_id
+    
+    b=""
+    if apacidadRoom[0]['3D'] == "yes":
+        b =". La sala cuenta con 3D"
+    else:
+        b =". La sala NO cuenta con 3D"
+    
+    
+    print('CapacidadRomm: ',capacidadRoom)
+    
+    
     return {
         'statusCode': 200,
-        'body': json.dumps(data)
+        'body': json.dumps(a+b)
+    }
+    
+def getPersonMovies(event, context):
+    print(json.dumps({"running": True}))
+    print(json.dumps(event))
+    
+    path = event["path"] # "/user/123"
+    array_path = path.split("/") # ["", "user", "123"]/ "/rooms/{room_id}/movies/{movie_id}"
+    person_id = array_path[-1]
+    
+    print("Llega hasta aca")
+    
+    q1 = table.scan(FilterExpression=Attr('sk').eq(person_id))
+    q2 = table.scan(FilterExpression=Attr('pk').begins_with("mov_") & Attr('sk').begins_with("room_"))
+    data1 = q1['Items']
+    data2 = q2['Items']
+    
+    temp = []
+    
+    for x in data1:
+        for y in data2:
+            if (x['schedule'] == y['schedule']) & (x['pk']==y['sk']):
+                q3=table.scan(FilterExpression=Attr('pk').eq(y['pk']) & Attr('sk').eq("mov_info"))
+                temp.append(q3['Items'][0])
+    
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps(temp)
+    }
+    
+    
+def putPeople(event, context):
+    print(json.dumps({"running": True}))
+    print(json.dumps(event))
+    
+    path = event["path"] # "/user/123"
+    array_path = path.split("/") # ["", "user", "123"]/ "/rooms/{room_id}/movies/{movie_id}"
+    room_id = array_path[-1]
+    
+    body = event["body"] #"{\n\t\"name\": \"Jack\",\n\t\"last_name\": \"Click\",\n\t\"age\": 21\n}"
+    body_object = json.loads(body)
+    
+    people = body_object
+    
+    print('people: ',people)
+    
+    q2 = table.scan(FilterExpression=Attr('pk').eq(room_id) & Attr('sk').begins_with("person_"))
+    asientosOcupados = len(q2['Items'])
+    
+    q1 = table.scan(FilterExpression=Attr('pk').eq(room_id) & Attr('sk').eq("room_info"))
+    
+    
+    
+    
+    print('room: ',q1['Items'])
+    print('room2: ',q1['Items'][0]['capacity'])
+    
+    capacidadRoom = q1['Items'][0]['capacity']
+    
+    print("Llega hasta despues de los qs")
+    
+    e=""
+    print('int(capacidadRoom)',int(capacidadRoom))
+    print('len(people)',len(people))
+    print('q1[Items]',q1['Items'])
+    print(len(q1['Items']))
+    
+    if (len(q1['Items'])>0) & ((int(capacidadRoom)-asientosOcupados)>len(people)):
+        print("ENTRA AL IF")
+        for x in people:
+            res = table.put_item(
+            Item={
+                'pk': x['pk'],
+                'sk': x['sk'],
+                'schedule': x['schedule'],
+                'last_name': x['last_name'],
+                'name': x['name']
+            })
+        e="Personas agregadas correctamente"
+    else:
+        e="No existen los asientos necesarios para acomodar a todas las personas o no se tienen los datos necesarios"
+    
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps(e)
     }
     
